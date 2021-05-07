d7722f3490b65833637cfbb8ed3df7091b77862d
Minecraft.GCB <1265723427@qq.com>
2021-05-07 18:33:52 +0800

    修正Report.java

--- src/main/java/plugins/wnplugin/Report.java
+++ src/main/java/plugins/wnplugin/Report.java
@@ -1,6 +1,13 @@
 package plugins.wnplugin;
 
+import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
 
 public class Report implements CommandExecutor {
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+
+        return false;
+    }
 }
